7a55df448fbd92d57e4a89c4ed6606c4fd6f11e3
Maximus Shymko <maximus.shymko@polymtl.ca>
2020-12-02 15:49:15 -0500

    Terminé

--- ch7.py
+++ ch7.py
@@ -2,23 +2,25 @@
 Exemple des notions du chapitre 7.
 """
 
-
 from chatbot import *
 from twitch_bot import *
 
 
 def build_say_hi_callback(bot, message):
-	# TODO: Créer et retourner une fonction qui prend un paramètre (ignoré).
-	#       Cette fonction envoie `message` dans le chat à l'aide de la méthode `send_privmsg` du paramètre `bot`.
-	pass
+    # TODO: Créer et retourner une fonction qui prend un paramètre (ignoré).
+    #       Cette fonction envoie `message` dans le chat à l'aide de la méthode `send_privmsg` du paramètre `bot`.
+    def callback(*args):
+        bot.send_privmsg(message)
+
+    return callback
+
 
 def run_ch7_example():
-	bot = TwitchBot("logs")
-	# TODO: Construire le callback avec le bot et un message de votre choix.
-	callback = ...
-	# TODO: Enregister le callback sous la commande "say_hi".
-	bot.register_command(...)
-	# TODO: Mettre votre jeton (incluant le "oauth:") et le nom du compte Twitch associé.
-	bot.connect_and_join("oauth:...", "...", "samsei109")
-	bot.run()
-
+    bot = TwitchBot("logs")
+    # TODO: Construire le callback avec le bot et un message de votre choix.
+    callback = build_say_hi_callback(bot, "bingus")
+    # TODO: Enregister le callback sous la commande "say_hi".
+    bot.register_command("say_hi", callback)
+    # TODO: Mettre votre jeton (incluant le "oauth:") et le nom du compte Twitch associé.
+    bot.connect_and_join("oauth:x6jjebg9q5leyxdu4khe18ea58uaea", "bingus_bot", "samsei109")
+    bot.run()
